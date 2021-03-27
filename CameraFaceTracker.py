72f5cd1eb5acd9a26c5dd394296b8295f90326a3
Daylend <Daylend@users.noreply.github.com>
2021-03-27 04:28:07 -0600

    Cleaned up some minor smells and added more descriptive comments

--- CameraFaceTracker.py
+++ CameraFaceTracker.py
@@ -4,19 +4,30 @@ import cv2
 import time
 import json
 
+# Address of the camera
 CAM_URL = "http://192.168.1.30/"
+# URL for grabbing a frame
 SNAPSHOT_URL = "snapshot.cgi"
+# URL for controlling the PTZ
 CONTROL_URL = "decoder_control.cgi"
+# Credentials (overwritten)
 CAM_USER = "admin"
 CAM_PASSWORD = "admin"
-PROTOTXT = "deploy.prototxt"
-WEIGHTS = "res10_300x300_ssd_iter_140000_fp16.caffemodel"
 CREDS_FILENAME = "camcreds.json"
+# Facial recognition parameters
+PROTOTXT = "deploy.prototxt"
+# Neural network model
+WEIGHTS = "res10_300x300_ssd_iter_140000_fp16.caffemodel"
+# Confidence threshold
 CONF_THRESH = 0.35
+# Non-maximum suppression threshold
 NMS_THRESH = 0.4
+# Bounding box to keep the face in
 TARGET_X = 150
 TARGET_Y = 130
+# Allow margin for error to avoid correcting too often
 TARGET_RADIUS = 10
+# How long each move command moves the camera for, and how long to wait between image polls
 WAIT_TIME = 500
 
 
@@ -27,20 +38,23 @@ PTZ_UP = 2
 PTZ_LEFT = 4
 PTZ_RIGHT = 6
 
-
-def movecamera(direction):
+# Move camera in specified direction
+def movecamera(direction, duration):
+    # Craft move and stop URLs for web API
     URL = CAM_URL + CONTROL_URL + "?" + "loginuse=" + CAM_USER + "&" + "loginpas=" + CAM_PASSWORD + \
           "&" + "command=" + str(int(direction)) + "&onestep=0"
     STOPURL = CAM_URL + CONTROL_URL + "?" + "loginuse=" + CAM_USER + "&" + "loginpas=" + CAM_PASSWORD + \
           "&" + "command=" + str(int(PTZ_STOP)) + "&onestep=0"
-    # print(URL)
+    # Send move command
     requests.get(URL)
     # convert wait time in ms to s then divide
-    time.sleep(WAIT_TIME/1000 / 2)
+    time.sleep(duration/1000 / 2)
+    # Send stop command
     requests.get(STOPURL)
 
 
 def main():
+    # Read credentials from file as dictionary
     creds = []
     with open(CREDS_FILENAME, "r") as camcreds:
         creds = json.load(camcreds)
@@ -49,13 +63,15 @@ def main():
     CAM_PASSWORD = creds["camPassword"]
 
     while True:
-
+        # Pull camera image from web API
         imgReq = requests.get(CAM_URL + SNAPSHOT_URL + "?" + "user=" + CAM_USER + "&" + "pwd=" + CAM_PASSWORD)
         imageBytes = np.asarray(bytearray(imgReq.content), dtype=np.uint8)
+        # Decode image to cv2 format
         image = cv2.imdecode(imageBytes, -1)
 
         net = cv2.dnn.readNetFromCaffe(PROTOTXT, WEIGHTS)
         (h, w) = image.shape[:2]
+        # Resize image and create blob
         blob = cv2.dnn.blobFromImage(cv2.resize(image, (300, 300)), 1.0, (300, 300), (104.0, 177.0, 123.0))
         net.setInput(blob)
         detections = net.forward()
@@ -63,6 +79,7 @@ def main():
         boxes = []
         confidences = []
 
+        # Draws boundary for display (optional)
         cv2.circle(image, (TARGET_X, TARGET_Y), TARGET_RADIUS, (0,255,0), 1)
 
         # shape function appears to list sizes of each dimension in the array
@@ -78,8 +95,8 @@ def main():
         # For loop is for multiple detections. We only need the most important detection.
         #for i in indices:
         if len(indices) >= 1:
-            i = indices[0]
-            i = i[0]
+            # Grab most important detection
+            i = indices[0][0]
             box = boxes[i]
             (startX, startY, endX, endY) = box.astype("int")
             y = startY - 10 if startY - 10 > 10 else startY + 10
@@ -96,20 +113,21 @@ def main():
             # centerYDiff, "radius: ", TARGET_RADIUS)
 
             # elif cause I only want to worry about one direction at a time
-            # check for turn right
+            # check for turn in each direction
             if centerXDiff > TARGET_RADIUS:
-                movecamera(PTZ_RIGHT)
+                movecamera(PTZ_RIGHT, WAIT_TIME)
                 #print("RIGHT")
             elif centerXDiff < -1*TARGET_RADIUS:
-                movecamera(PTZ_LEFT)
+                movecamera(PTZ_LEFT, WAIT_TIME)
                 #print("LEFT")
             elif centerYDiff > TARGET_RADIUS:
-                movecamera(PTZ_UP)
+                movecamera(PTZ_UP, WAIT_TIME)
                 #print("UP")
             elif centerYDiff < -1*TARGET_RADIUS:
-                movecamera(PTZ_DOWN)
+                movecamera(PTZ_DOWN, WAIT_TIME)
                 #print("DOWN")
 
+            # Print confidences and facial recognition boxes to display (optional)
             text = "{:.2f}%".format(confidences[i]*100)
             cv2.rectangle(image, (startX, startY), (endX, endY), (0, 0, 255), 2)
             cv2.rectangle(image, (5+centerX, 5+centerY), (-5+centerX, -5+centerY), (0, 255, 0), 2)
